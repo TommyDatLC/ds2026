1821eab0297a8bd3c656d84562be6b407341675a
TommyDatLC <lolgaming29082004@gmail.com>
2025-11-25 21:13:37 +0700

    Full lab1

--- Prac1/server.py
+++ Prac1/server.py
@@ -1,18 +1,19 @@
 import socket
-import Utils
+import Utils 
 
 def server_route(client : socket.socket):
     route_byte = client.recv(1)
     if (route_byte == Utils.SERVER_UPLOAD):
-        server_upload()
+        server_upload(client)
     else:
-        server_download()
-
-def server_download():
+        server_download(client)
+    client.close()
+    
+def server_download(client : socket.socket):
     filename = Utils.ReceiveFileName(client,False)
     Utils.send_file_sep(filename,client)
 
-def server_upload():
+def server_upload(client : socket.socket):
     filename = Utils.ReceiveFileName(client)
     Utils.recv_file_sep(filename,client)
 
@@ -20,7 +21,6 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('0.0.0.0', Utils.port)) 
 server.listen()
 while True:
-
     print("Đang ngồi hóng drama...")
     client, addr = server.accept()
     print("Ket noi thanh cong")
